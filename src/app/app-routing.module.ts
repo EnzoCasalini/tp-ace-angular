4040b7057eb2af4ecc0cdeed83ff74a889894f22
Andréas CASTELLO <andreas.castello06@gmail.com>
2023-05-15 12:30:41 +0200

    Panier + Commande (sans article)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsListComponent} from "./products-list/products-list.component";
 import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {PanierComponent} from "./panier/panier.component";
+import {CommandeComponent} from "./commande/commande.component";
+import {ErreurComponent} from "./erreur/erreur.component";
 
 const routes: Routes = [
   { path: '', component: ProductsListComponent},
-  { path: 'product/:id', component: ProductDetailComponent}
+  { path: 'product/:id', component: ProductDetailComponent},
+  { path: 'panier', component: PanierComponent},
+  { path: 'commande/:prenom/:id', component: CommandeComponent },
+  { path: 'erreur', component: ErreurComponent },
 ];
 
 @NgModule({
