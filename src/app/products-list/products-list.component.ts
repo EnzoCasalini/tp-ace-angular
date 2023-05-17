026ad9475d7ea3b2a7e13756da610b79e7db0154
ChandlerEN <chandler.nguyen@ynov.com>
2023-05-18 00:49:08 +0200

    General CSS modification + pipes

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -6,13 +6,13 @@ import {Observable} from "rxjs";
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
-  styles: [
-  ]
+  styleUrls: ['product-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
   search: string = "";
   title: string = "";
   productsObservable : Observable<any>;
+  order: string = "asc";
 
   constructor(private productsService: ProductsService) {
     this.productsObservable = new Observable<[]>;
@@ -22,5 +22,9 @@ export class ProductsListComponent implements OnInit {
     this.search = "";
     this.productsObservable = this.productsService.getAllProducts();
     this.title = "my HP app"
+  }
+
+  SortOrder() {
+    (this.order == "asc") ? this.order = "desc" : this.order = "asc";
   }
 }
