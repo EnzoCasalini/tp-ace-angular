2686f94a3b30eeb31960c94e17d9135344bac642
ChandlerEN <chandler.nguyen@ynov.com>
2023-05-18 03:19:18 +0200

    CSS completed + few code improvements

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {Product} from "../models/product.model";
 import {ProductsService} from "../services/products.service";
 import {Observable} from "rxjs";
 
